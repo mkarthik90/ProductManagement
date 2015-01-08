7047c3c83bad25b6eab8c2bceb676f6ee9254129
vigneshkumar94 <wikeykumar@gmail.com>
2015-01-08 11:50:22 +0530

    Revert "de-registration"
    This reverts commit a7135423d1fd124a2f7845c85309f708096c2d46.

--- ProductMaintanance/src/com/witty/dao/IDeregistrDAO.java
+++
@@ -1,8 +0,0 @@
-package com.witty.dao;
-
-public interface IDeregistrDAO {
-	public int getDeregistrDetails(String Empid,String userName);
-	public int getremprodDetails(String Empid);
-	public void deletemp(String Empid);
-
-}
--- ProductMaintanance/src/com/witty/service/IDeregistrService.java
+++
@@ -1,7 +0,0 @@
-package com.witty.service;
-
-public interface IDeregistrService {
-
-	public boolean checkregistrDetails(String Empid, String userName);
-	public boolean checkprodDetails(String Empid,String userName);
-}
